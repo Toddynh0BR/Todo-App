81d8850d3323045b0efd8ee28f92d5e0691e64fb
Matheus <galaxyplay41@gmail.com>
2024-05-20 14:06:28 -0300

    CSS Finalizado

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 import './js/elements.js';
+
+
+const btnSun= document.querySelector('.btnSun');
+const btnMoon= document.querySelector('.btnMoon')
+
+
+
+btnSun.addEventListener('click', ()=> {
+  document.documentElement.classList.toggle('dark');
+})
+btnMoon.addEventListener('click', ()=> {
+ document.documentElement.classList.toggle('dark');
+})
